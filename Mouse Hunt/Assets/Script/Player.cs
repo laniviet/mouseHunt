b1c8b4b5170a8de5e6d1e947b82208e9011fac7d
laniviet <laniviet@gmail.com>
2018-04-10 12:56:14 -0700

    Added project with game scene: player movement and block spawner

---
+++ Mouse Hunt/Assets/Script/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;      //Allows us to use SceneManager
+using System.Collections.Generic;
+
+//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+public class Player : MonoBehaviour
+{
+    public float speed;             //Floating point variable to store the player's movement speed.
+    private float halfSpd;
+
+    public bool hitUp;
+
+    private Rigidbody2D rb;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    public Transform UCol;
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb = GetComponent<Rigidbody2D>();
+
+        halfSpd = speed / 2;
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        InputMovement();
+
+        hitUp = Physics2D.Linecast(transform.position, UCol.position, 1 << LayerMask.NameToLayer("moveBlock"));
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += transform.up * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        InputDiagonalMove();
+    }
+
+    void InputDiagonalMove()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += transform.up * halfSpd * Time.deltaTime;
+            transform.position += transform.right * halfSpd * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += transform.up * halfSpd * Time.deltaTime;
+            transform.position -= transform.right * halfSpd * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)) {
+            transform.position -= transform.up * halfSpd * Time.deltaTime;
+            transform.position += transform.right * halfSpd * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= transform.up * halfSpd * Time.deltaTime;
+            transform.position -= transform.right * halfSpd * Time.deltaTime;
+        }
+    }
+}
---
+++ Mouse Hunt/Assets/Script/spawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+    public GameObject block;
+
+    public List<bool> Spawners;
+
+    public int spawnNum;
+    public float timer;
+    public float timerMax;
+    public bool spawn;
+    public bool spawnGo;
+
+    public string type;
+    int num = 0;
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i< Spawners.Count; i++)
+        {
+            int nameNum = i;
+            if(this.gameObject.name == nameNum.ToString())
+            {
+                Spawners[i] = true;
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(spawnNum > 0)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= timerMax)
+        {
+            spawn = true;
+            timer = 0;
+            spawnBlock();
+        }
+		
+	}
+
+    void spawnBlock()
+    {
+        if (spawnGo == true)
+        {
+            for (int i = 0; i < Spawners.Count; i++) //Spawners.Count check for size of element
+            {
+                if (type[num] == '0') //takes input of string in the inspector
+                {
+                    Instantiate(block, transform.position, Quaternion.identity);
+                    spawnNum -= 1;
+                }
+            }
+        }
+    }
+}
